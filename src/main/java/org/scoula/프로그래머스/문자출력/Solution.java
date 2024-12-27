04a7cf1bf6f88306f1c02bcb26fbc7730d71a7cd
이동훈 <zkwlao0@naver.com>
2024-12-27 10:24:27 +0900

    프로그래머스 문자출력

---
+++ src/main/java/org/scoula/프로그래머스/문자출력/Solution.java
@@ -0,0 +1,12 @@
+package org.scoula.프로그래머스.문자출력;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n 2\n 1"); // \n은 줄바꿈을 뜻한다
+                System.out.println(message);
+    }
+}
