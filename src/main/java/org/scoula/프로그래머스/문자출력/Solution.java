4a7b72b24d629c65f4e496327144a2502a4c0792
이동훈 <zkwlao0@naver.com>
2025-01-02 15:35:02 +0900

    프로그래머스 각도합치기

--- src/main/java/org/scoula/프로그래머스/문자출력/Solution.java
+++ src/main/java/org/scoula/프로그래머스/문자출력/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public static void main(String[] args) {
         String message = "Let's go!";
 
-        System.out.println("3\n 2\n 1"); // \n은 줄바꿈을 뜻한다
+     System.out.println("3\n 2\n 1"); // \n은 줄바꿈을 뜻한다
                 System.out.println(message);
     }
 }
