2035d7814484ef068e25291d22d0b1688fe3e00f
이동훈 <zkwlao0@naver.com>
2025-01-13 15:08:59 +0900

    프로그래머스 물부족

---
+++ src/main/java/org/scoula/프로그래머스/물부족/Solution.java
@@ -0,0 +1,16 @@
+package org.scoula.프로그래머스.물부족;
+
+public class Solution {
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for(int i=0; i<change.length; i++){
+            usage = usage + (usage * change[i] / 100);
+            total_usage += usage;
+            if(total_usage > storage){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
