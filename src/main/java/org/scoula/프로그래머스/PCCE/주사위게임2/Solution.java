4978e1c1bbaaf7639edecb9c3be08b4289f1f272
이동훈 <zkwlao0@naver.com>
2025-01-21 15:45:13 +0900

    프로그래머스 주사위게임2

---
+++ src/main/java/org/scoula/프로그래머스/PCCE/주사위게임2/Solution.java
@@ -0,0 +1,17 @@
+package org.scoula.프로그래머스.PCCE.주사위게임2;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
+        if(a==b&&b==c){
+            answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }else if(a==b&&b!=c||a==c&&c!=b||a!=b&&b==c){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else{
+            answer = a+b+c;
+        }
+
+        return answer;
+    }
+}
