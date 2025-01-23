dc7b3086d655b86053a156fe06eeab2eadc29451
이동훈 <zkwlao0@naver.com>
2025-01-23 14:38:58 +0900

    프로그래머스 출력

---
+++ src/main/java/org/scoula/프로그래머스/PCCE/출력/Solution.java
@@ -0,0 +1,16 @@
+package org.scoula.프로그래머스.PCCE.출력;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
+
