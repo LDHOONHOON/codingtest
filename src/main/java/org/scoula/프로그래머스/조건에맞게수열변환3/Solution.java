262b5d0e65dd9b833f189ddd7ce152927511fed5
이동훈 <zkwlao0@naver.com>
2025-02-12 09:52:22 +0900

    프로그래머스 조건에 맞게 수열 변환하기3

---
+++ src/main/java/org/scoula/프로그래머스/조건에맞게수열변환3/Solution.java
@@ -0,0 +1,17 @@
+package org.scoula.프로그래머스.조건에맞게수열변환3;
+
+public class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+            if(k%2==0){
+                answer[i]=arr[i]+k;
+            }else{
+                answer[i]=arr[i]*k;
+            }
+        }
+
+        return answer;
+    }
+}
