34a3a2e573111bab532a974e33cb2414ba406fe9
이동훈 <zkwlao0@naver.com>
2025-02-13 11:08:35 +0900

    프로그래머스 뒤에서5등까지

---
+++ src/main/java/org/scoula/프로그래머스/뒤에서5등까지/Solution.java
@@ -0,0 +1,14 @@
+package org.scoula.프로그래머스.뒤에서5등까지;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Arrays.sort(num_list);
+        for(int i=0; i<5;i++){
+            answer[i] = num_list[i];
+        }
+        return answer;
+    }
+}
