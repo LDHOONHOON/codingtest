c4c2a87dd9bab1afc7e76a5db3a727e116cacf03
이동훈 <zkwlao0@naver.com>
2025-02-17 11:21:47 +0900

    콜라츠수열 만들기

---
+++ src/main/java/org/scoula/프로그래머스/콜라츠수열만들기/Solution.java
@@ -0,0 +1,25 @@
+package org.scoula.프로그래머스.콜라츠수열만들기;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int n) {
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(n);
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            sequence.add(n);
+        }
+
+        int[] answer = new int[sequence.size()];
+        for (int i = 0; i < sequence.size(); i++) {
+            answer[i] = sequence.get(i);
+        }
+        return answer;
+    }
+}
