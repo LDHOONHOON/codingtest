be1fcc12eda8c3e530ba9e18f8a0b67b1b8aa1b1
이동훈 <zkwlao0@naver.com>
2025-03-31 15:11:46 +0900

    프로그래머스 7의개수

---
+++ src/main/java/org/scoula/프로그래머스/칠의개수/Solution.java
@@ -0,0 +1,16 @@
+package org.scoula.프로그래머스.칠의개수;
+
+import java.util.*;
+
+public class Solution{
+    public int solution(int[] array){
+        int answer = 0;
+        String str = Arrays.toString(array);
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '7'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
