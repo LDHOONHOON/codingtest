860d6f0be5446ad6780f77ef200e0388aee766c9
이동훈 <zkwlao0@naver.com>
2025-06-20 16:46:04 +0900

    프로그래머스 저주의 숫자3

---
+++ src/main/java/org/scoula/프로그래머스/저주의숫자3/Solution.java
@@ -0,0 +1,36 @@
+package org.scoula.프로그래머스.저주의숫자3;
+
+//풀이1
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer++;
+            while (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+//풀이2
+//public class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//        int count = 0;
+//        int i = 1;
+//
+//        while(count != n){
+//            if(i % 3 != 0 && i % 10 != 3 && (i / 10) % 10 != 3){
+//                answer = i;
+//                count++;
+//            }
+//
+//            i++;
+//        }
+//
+//        return answer;
+//    }
+//}
