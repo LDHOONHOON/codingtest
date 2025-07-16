d6d0c4587d74a15e4d545c33588a4916e10443cf
이동훈 <zkwlao0@naver.com>
2025-07-16 15:50:40 +0900

    프로그래머스 rny_string

---
+++ src/main/java/org/scoula/프로그래머스/rny_string/Solution.java
@@ -0,0 +1,9 @@
+package org.scoula.프로그래머스.rny_string;
+
+public class Solution {
+    public String solution(String rny_string) {
+        String answer = rny_string.replace("m", "rn");
+
+        return answer;
+    }
+}
