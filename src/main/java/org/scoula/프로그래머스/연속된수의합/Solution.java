4a5aba56ec2ae440e6981c5004f26911eee07b78
이동훈 <zkwlao0@naver.com>
2025-07-30 11:07:16 +0900

    프로그래머스 연속된 두 수의 합

---
+++ src/main/java/org/scoula/프로그래머스/연속된수의합/Solution.java
@@ -0,0 +1,14 @@
+package org.scoula.프로그래머스.연속된수의합;
+
+public class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int start = (total * 2 / num - num + 1) / 2; // 시작 값 계산
+        for (int i = 0; i < num; i++) {
+            answer[i] = start + i;
+        }
+        return answer;
+    }
+}
+
+
