8451acf4347e5f18b659e553f47dcbc5c3de770e
이동훈 <zkwlao0@naver.com>
2025-08-11 17:03:53 +0900

    프로그래머스 점의 위치 구하기

---
+++ src/main/java/org/scoula/프로그래머스/점의위치구하기/Solution.java
@@ -0,0 +1,17 @@
+package org.scoula.프로그래머스.점의위치구하기;
+
+public class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        if (dot[0] > 0 && dot[1] > 0) {
+            answer = 1;
+        } else if (dot[0] < 0 && dot[1] > 0) {
+            answer = 2;
+        } else if (dot[0] < 0 && dot[1] < 0) {
+            answer = 3;
+        } else {
+            answer = 4;
+        }
+        return answer;
+    }
+}
